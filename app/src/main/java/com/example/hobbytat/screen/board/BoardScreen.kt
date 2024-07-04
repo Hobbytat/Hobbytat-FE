00e955b55ce773b48650d668a17a88fd1de4b901
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-07-04 20:58:31 +0900

    build: 프로젝트 초기 세팅

---
+++ app/src/main/java/com/example/hobbytat/screen/board/BoardScreen.kt
@@ -0,0 +1,8 @@
+package com.example.hobbytat.screen.board
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BoardScreen() {
+    
+}
---
+++ app/src/main/java/com/example/hobbytat/screen/sign/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.example.hobbytat.screen.sign
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+
+}
