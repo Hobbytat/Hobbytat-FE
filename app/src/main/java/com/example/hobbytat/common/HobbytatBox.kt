cbf1760a204b7ea203456928536dc8b9e3b344af
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-07-06 07:00:28 +0900

    feat: api 계속 연동중..

--- app/src/main/java/com/example/hobbytat/common/HobbytatBox.kt
+++ app/src/main/java/com/example/hobbytat/common/HobbytatBox.kt
@@ -11,22 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.hobbytat.R
+import coil.compose.rememberImagePainter
 
 @Composable
 fun HobbytatBox(
-//    img: String,
+    img: String,
     title: String,
     onClick: () -> Unit
 ) {
     Column {
         Image(
-            painter = painterResource(id = R.drawable.artist_bg), contentDescription = "하비탯이미지",
+            painter = rememberImagePainter(data = img),
+            contentDescription = "하비탯이미지",
             modifier = Modifier
                 .clip(RoundedCornerShape(12.dp))
                 .clickable(onClick = onClick)
@@ -35,13 +34,5 @@ fun HobbytatBox(
         Spacer(modifier = Modifier.height(12.dp))
         Text(text = title, fontSize = 16.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(30.dp))
-    }
-}
-
-@Preview
-@Composable
-private fun Preview() {
-    HobbytatBox("예술가의 하비탯") {
-
     }
 }
